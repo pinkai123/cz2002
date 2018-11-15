6f04a2e5316724ce24b85feda21d011cbcb7c206
jesslyn <jesslyn.chew.s.y@gmail.com>
2018-11-16 07:15:45 +0800

    remove

--- LocalAssignment2/src/others/Existing.java
+++
@@ -1,8 +0,0 @@
-package others;
-
-public class Existing {
-	// Where no existence will result in an error (if not accounted for)
-	public static boolean studentExist(String matric) {
-		return true;
-	}
-}
