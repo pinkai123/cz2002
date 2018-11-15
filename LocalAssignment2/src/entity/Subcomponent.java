2d68f0028512a70e118df5603297153a3abf2e2e
jesslyn <jesslyn.chew.s.y@gmail.com>
2018-11-16 07:40:09 +0800

    javadoc

--- LocalAssignment2/src/entity/Subcomponent.java
+++ LocalAssignment2/src/entity/Subcomponent.java
@@ -2,15 +2,41 @@ package entity;
 
 import java.util.ArrayList;
 
+/**
+ * Represent the subcomponents of the coursework mark,
+ * consisting of a weightage and a name to identity itself
+ * from other subcomponents.
+ * @author SS5 Group 4
+ * @version 1.0
+ * @since 2018-11-15
+ */
 public class Subcomponent {
+	/**
+	 * The percentage of the subcomponent which determines
+	 * the marks and weightage of coursework
+	 */
 	private double percentage;
+	/**
+	 * The name of the subcomponent
+	 */
 	private String name;
 
+	/**
+	 * Creates a new subcomponent with given name and percentage.
+	 * @param name The subcomponent's name
+	 * @param percentage The subcomponent's weightage under coursework
+	 */
 	public Subcomponent( String name,double percentage){
 		this.percentage = percentage;
 		this.name  = name;
 	}
-	//verification
+	
+
+	/**
+	 * Check whether all subcomponent in weightage adds up to 1.
+	 * @param subPercentage All the subcomponents' percentage 
+	 * @return true if subcomponents' percentage in this weightage adds up to 1
+	 */
 	public static int verificationSubcomponentPercentage(ArrayList<Double> subPercentage) {
 		double percentage = 0;
 		for (int i = 0; i < subPercentage.size(); i ++) {
@@ -24,6 +50,12 @@ public class Subcomponent {
 			return 1;
 	}
 	
+	/**
+	 * Check whether there a subcomponent name is the same as any of the other subcomponent.
+	 * @param subcomponent other subcomponents' name
+	 * @param name a subcomponent name
+	 * @return false if there is a match in subcomponent names
+	 */
 	public static boolean verificationSubcomponentName(ArrayList<String> subcomponent, String name) {
 		for(int i= 0;i<subcomponent.size();i++) {
 			if(subcomponent.get(i).equals(name)) {
@@ -34,15 +66,31 @@ public class Subcomponent {
 	}
 	
 	// Accessors
+	/**
+	 * Get the percentage of the subcomponent.
+	 * @return This subcomponent's percentage
+	 */
 	public double getPercentage() { return percentage;}
+	/**
+	 * Get the name of the subcomponent.
+	 * @return This subcomponent's name
+	 */
 	public String getName() { return name;}
 	
 	
 	
 	// Mutators
+	/**
+	 * Changes the percentage of the subcomponent.
+	 * @param percentage New subcomponent's percentage
+	 */
 	public void setPercentage(double percentage){
 		this.percentage = percentage;
 	}
+	/**
+	 * Changes the name of the subcomponent.
+	 * @param name New subcomponent's name
+	 */
 	public void setName(String name){
 		this.name = name;
 	}
